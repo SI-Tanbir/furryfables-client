2048688760cd1f93bde908347433b6bb6e236b4f
SI-tanbir <si017490@gmail.com>
2024-12-24 18:57:25 +0600

    adding aos animation

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -6,46 +6,44 @@ import img3 from "./assets/3.png";
 import img4 from "./assets/4.webp";
 import img5 from "./assets/5.webp";
 import img6 from "./assets/6.webp";
-import img11 from './assets/11.webp'
+import img11 from "./assets/11.webp";
+
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 import data from "./fakedb.json";
 
 // console.log(data)
 
-
 const HomePage = () => {
+  useEffect(() => {
+    Aos.init({
+      duration: 1200, // Animation duration in milliseconds
+      once: true, // Whether the animation happens only once
+    });
+  }, []);
 
-  const [fakedata,setFakeData]=useState([])
-  useEffect(()=>{
+  const [fakedata, setFakeData] = useState([]);
+  useEffect(() => {
+    setFakeData(data.slice(0, 9));
+  }, []);
 
-
-    setFakeData(data.slice(0,9))
-
-
-  },[])
-
-  const handleAddcart=(id)=>{
-
+  const handleAddcart = (id) => {
     const cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-     // Check if the product is already in the cart
-  if (!cart.includes(id)) {
-    cart.push(id); // Add the product ID to the cart
-  }
+    // Check if the product is already in the cart
+    if (!cart.includes(id)) {
+      cart.push(id); // Add the product ID to the cart
+    }
 
+    // Save the updated cart back to local storage
+    localStorage.setItem("cart", JSON.stringify(cart));
 
-
-  // Save the updated cart back to local storage
-  localStorage.setItem("cart", JSON.stringify(cart));
-
-  console.log(`Product ${id} added to cart`, cart);
-
+    console.log(`Product ${id} added to cart`, cart);
 
     // console.log(id)
+  };
 
-
-  }
-  
   return (
     <div>
       {/* adding carousel */}
@@ -99,17 +97,21 @@ const HomePage = () => {
       </div>
 
       {/* starting of gallery section */}
+      {/* data-aos="fade-right" */}
 
-      <button className="text-4xl font-pacifico p-4 rounded my-8 mb-0 bg-customRed ">
-        Adorable, Soft, and Full of Love
-      </button>
+      <div data-aos="fade-right">
+        <button className="text-4xl font-pacifico p-4 rounded my-8 mb-0 bg-customRed ">
+          Adorable, Soft, and Full of Love
+        </button>
+      </div>
+
       <div className="flex gap-4 ">
-        <div className="relative">
+        <div data-aos="fade-right" className="relative">
           <img src={img3} className=" rounded-lg h-full w-[1000px]" alt="" />
           {/* <button className='btn absolute bg-[#FB9EC6] border-none top-[70%] left-5  text-4xl '>Shop Now</button> */}
         </div>
 
-        <div>
+        <div data-aos="fade-left">
           <img src={img4} className="rounded-xl" alt="" />
           {/* <img src={img2} className='w-[50%] h-[50%]' alt="" /> */}
         </div>
@@ -117,42 +119,39 @@ const HomePage = () => {
 
       {/* <img src={img6} className='w-full rounded-xl' alt="" /> */}
 
+      <div data-aos="flip-up">
+        <button className="text-4xl font-pacifico  rounded my-8 mb-0 bg-customRed btn ">
+          All Favourites Toys
+        </button>
+      </div>
 
-
-
-      <button className="text-4xl font-pacifico  rounded my-8 mb-0 bg-customRed btn " >All Favourites Toys</button>
       <div className="grid grid-cols-3 gap-8 m-4">
-        
-
-      {fakedata.map((res) => (
-
-
-
-<div key={res.id}>
-  <div className="card card-compact bg-base-100 w-[400px]  shadow-xl">
-    <figure>
-      <img className=" "
-        src={res.imageUrl}
-        alt="Shoes"
-      />
-    </figure>
-    <div className="card-body">
-      <h2 className="card-title">{res.name}</h2>
-      <p>{res.description}</p>
-      <div className="card-actions justify-between ">
-        <button className="btn bg-customRed">Buy Now</button>
-        <button onClick={()=>handleAddcart(res.id)} className="btn bg-customRed ">Add to cart</button>
+        {fakedata.map((res) => (
+          <div key={res.id}>
+            <div
+              data-aos="zoom-in"
+              className="card card-compact bg-base-100 w-[400px]  shadow-xl"
+            >
+              <figure>
+                <img className=" " src={res.imageUrl} alt="Shoes" />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">{res.name}</h2>
+                <p>{res.description}</p>
+                <div className="card-actions justify-between ">
+                  <button className="btn bg-customRed">Buy Now</button>
+                  <button
+                    onClick={() => handleAddcart(res.id)}
+                    className="btn bg-customRed "
+                  >
+                    Add to cart
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
-    </div>
-  </div>
-</div>
-
-))}
-
-
-      </div>
-
-
     </div>
   );
 };
